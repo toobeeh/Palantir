f49783bd69d3938272ecfa7a7a47f41a55317cd4
tobeh <tobiasch@gmx.at>
2020-05-29 15:46:39 +0200

    Projektdateien hinzufügen.

---
+++ Palantir/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using System.Threading.Tasks;
+
+namespace Palantir
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting palantir...");
+            var discordClient = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NzE1ODc0Mzk3MDI1NDY4NDE3.XtDksg.vbCY4jq50WGZthP2aZrIBIqzS7Q",
+                TokenType = TokenType.Bot
+            });
+
+            discordClient.MessageCreated += OnMessageCreated;
+
+            await discordClient.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task OnMessageCreated(MessageCreateEventArgs e)
+        {
+            if (string.Equals(e.Message.Content, "hello", StringComparison.OrdinalIgnoreCase))
+            {
+                await e.Message.RespondAsync(e.Message.Author.Username);
+            }
+        }
+    }
+}
