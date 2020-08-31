975f88bfd156872ba072bc01a7c4ccb657a45eed
tobeh <tobiasch@gmx.at>
2020-08-31 15:55:10 +0200

    Change update info

--- Tether.cs
+++ Tether.cs
@@ -223,7 +223,7 @@ namespace Palantir
                 }
             });
 
-            message += "```css\nUpdate info: Collect bubbles to unlock new sprites!\nTry out the bot commands '>inventory' and '>bubbles'!```\nBe the first to catch a drop and get extra Bubbles!\n\n";
+            message += "```arm\nUpdate info: Collect bubbles and catch Drop to unlock new sprites!\nTry out the bot command '>bubbles'!```\n\n";
 
             message += PalantirSettings.Header + "\n";
             if(PalantirSettings.ShowRefreshed) message += "Refreshed: " + DateTime.UtcNow.AddHours(PalantirSettings.Timezone).ToShortTimeString() + " (UTC " + PalantirSettings.Timezone.ToString("+0;-#") + ")\n"; 
