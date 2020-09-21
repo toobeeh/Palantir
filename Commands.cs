1f12e887e86c5dfb1296e5026c6454b0c73196d2
tobeh <tobiasch@gmx.at>
2020-09-21 13:37:44 +0200

    Initial stat test

--- Commands.cs
+++ Commands.cs
@@ -481,6 +481,21 @@ namespace Palantir
             await context.Channel.SendMessageAsync(embed: embed);
         }
 
+        [Description("Show bubble gain statistics")]
+        [Command("stat")]
+        public async Task Statistic(CommandContext context)
+        {
+            string login = BubbleWallet.GetLoginOfMember(context.Message.Author.Id.ToString());
+            Tracer.BubbleTrace trace = new Tracer.BubbleTrace(login, 10);
+            DiscordEmbedBuilder embed = new DiscordEmbedBuilder();
+            embed.Title = "🔮  How to Bubble ";
+            embed.Color = DiscordColor.Magenta;
+            embed.AddField("What are Bubbles?", "Bubbles are a fictional currency of the Palantir Bot.\nWhen you're connected to the Bot, you will be rewarded 1 Bubble every 10 seconds.\nBubbles are used to buy Sprites which other users of the Skribbl-Typo extension can see in your Skribbl avatar.\nOnce in a while, on the skribbl canvas will apear a drop icon - the player who clicks it first is rewarded a Drop to their inventory.\nA Drop is worth 50 Bubbles and adds up to your Bubble credit.");
+            embed.AddField("Commands", "➜ `>inventory` List your Sprites and Bubble statistics.\n➜ `>sprites` Show all buyable Sprites.\n➜ `>sprites [id]` Show a specific Sprite.\n➜ `>buy [id]` Buy a Sprite.\n➜ `>use [id]` Select one of your Sprites.\n➜ `>leaderboard` Show your server's leaderboard.\n➜ `>calc` Calculate various things.");
+
+            await context.Channel.SendMessageAsync(embed: embed);
+        }
+
         [Description("Fancy calculation stuff")]
         [Command("calc")]
         public async Task Calc(CommandContext context, [Description("Calc mode: bubbles, rank or sprite")]string mode="", [Description("Whatever fits your mode.")] double target=0)
--- Tracer/Tracer.cs
+++ Tracer/Tracer.cs
@@ -44,4 +44,31 @@ namespace Palantir.Tracer
             dbcontext.Dispose();
         }
     }
+
+    public class BubbleTrace
+    {
+        public Dictionary<DateTime, int> History { get; private set; }
+        public BubbleTrace(string login, int dayLimit)
+        {
+            History = new Dictionary<DateTime, int>();
+            PalantirDbContext context = new PalantirDbContext();
+            List<BubbleTraceEntity> traces = context.BubbleTraces.Where(t => t.Login == login && Convert.ToDateTime(t.Date) <= DateTime.UtcNow.AddDays(dayLimit * -1)).ToList();
+            Dictionary<DateTime, int> combined = new Dictionary<DateTime, int>();
+            for(int daysAgo = dayLimit; daysAgo >= 0; daysAgo--)
+            {
+                DateTime historyPoint = DateTime.Now.AddDays(-1 * daysAgo);
+                traces.Sort((first, second) =>
+                {
+                    DateTime firstD = Convert.ToDateTime(first.Date);
+                    DateTime secondD = Convert.ToDateTime(second.Date);
+                    if ((firstD - historyPoint).Days == 0 || (firstD - historyPoint).Days < 0 && firstD < secondD) return -1;
+                    else if ((secondD - historyPoint).Days == 0 || (secondD - historyPoint).Days < 0 && secondD < firstD) return 1;
+                    else return -1;
+                });
+                History.Add(Convert.ToDateTime(traces.First().Date), traces.First().Bubbles);
+            }
+            context.Dispose();
+        }
+    }
+
 }
