bdc5c47e9db1093de2d29a3f5a8de416c601dd0d
tobeh <tobiasch@gmx.at>
2020-12-21 19:29:49 +0100

    Add commit message

--- Commands.cs
+++ Commands.cs
@@ -988,6 +988,7 @@ namespace Palantir
             }
 
             string upd = "git -C /home/pi/Palantir pull".Bash();
+            upd += "\n\n Latest commit: " + "git log--oneline -1".Bash();
             await Program.SendEmbed(context.Channel, "[literally dies...]", "You made me do this!!!\n\n**Update result:**\n"+upd);
             string op = "sudo service palantir restart".Bash();
             Environment.Exit(0);
