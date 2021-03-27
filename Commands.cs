acb15c34e2dfbe87f99b9bae82d6c294b9e4e381
tobeh <tobiasch@gmx.at>
2021-03-27 20:34:29 +0100

    Dont stalk

--- Commands.cs
+++ Commands.cs
@@ -640,7 +640,8 @@ namespace Palantir
         {
             Program.Feanor.ValidateGuildPalantir(context.Guild.Id.ToString());
             Program.Feanor.UpdateMemberGuilds();
-            if (!Program.Feanor.PalantirMembers.Any(member => member.Guilds.Any(guild => guild.GuildID == context.Guild.Id.ToString())))
+            if (!Program.Feanor.PalantirMembers.FirstOrDefault(member => member.UserID == context.User.Id.ToString())
+                .Guilds.Any(guild => guild.GuildID == context.Guild.Id.ToString()))
             {
                 await Program.SendEmbed(context.Channel, "Uh oh, caught you stalking!", "Connect to this discord server to use this command here.");
                 return;
