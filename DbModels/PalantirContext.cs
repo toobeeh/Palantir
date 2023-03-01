f21779eb1bbf0670d09343decba4b9f4bdfcfc7b
tobeh <tobiasch@gmx.at>
2023-03-01 21:21:49 +0100

    use vultr db

--- DbModels/PalantirContext.cs
+++ DbModels/PalantirContext.cs
@@ -68,7 +68,7 @@ namespace Palantir.Model
 
         public virtual DbSet<Webhook> Webhooks { get; set; }
 
-        private readonly string conn = "server=localhost;user id=palantir;database=palantir";
+        private readonly string conn = "server=108.61.190.186;user id=palantir;database=palantir";
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             // => options.UseSqlite("Data Source=" + path);
             => options.UseMySql(conn, ServerVersion.AutoDetect(conn));
