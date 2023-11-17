39a4f2d9e4e421f51c0e5be810968406c8bf17c7
tobeh <tobiasch@gmx.at>
2023-11-17 22:10:32 +0100

    idk

---
+++ DbModels/CloudTag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Palantir.Model
+{
+    public partial class CloudTag
+    {
+        public int Owner { get; set; }
+        public long ImageId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool Own { get; set; }
+        public long Date { get; set; }
+        public string Language { get; set; }
+        public bool Private { get; set; }
+    }
+}
---
+++ DbModels/Lob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Palantir.Model
+{
+    public partial class Lob
+    {
+        public string PlayerLobbyId { get; set; }
+        public string JsonUnquoteDcName { get; set; }
+        public string NameExp3 { get; set; }
+    }
+}
